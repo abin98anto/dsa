db92b682bc3b57b4ee410219e92e7259b07d60ae
abin98anto <abin98anto@gmail.com>
2025-04-29 01:19:10 +0530

    linked list added

---
+++ w1/LinkedList.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append = (data) => {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // this.tail.next = newNode;
+      // this.tail = newNode;
+
+      let cur = this.head;
+      while (cur.next) {
+        cur = cur.next;
+      }
+      cur.next = newNode;
+    }
+    this.size++;
+  };
+
+  reverse = () => {
+    let cur = this.head;
+    let pre = null;
+    let nxt = null;
+
+    while(cur) {
+        nxt = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = nxt;
+    }
+
+    this.tail = this.head;
+    this.head = pre;
+  }
+
+  print = () => {
+    let result = "";
+    let cur = this.head;
+    while (cur) {
+      result += `${cur.data} -> `;
+      cur = cur.next;
+    }
+    console.log(result + "null");
+  };
+}
+
+console.log("linked list");
+const LL = new LinkedList();
+LL.append(1);
+LL.append(2);
+LL.append(3);
+LL.print();
+LL.reverse();
+LL.print();
